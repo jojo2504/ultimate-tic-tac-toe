bc6e895e9290e197e762107d9f20aa2467b129b6
Jojo <jojo.duchuy@gmail.com>
2026-04-23 13:36:40 +0200

    feat: engine can now play first in engien vs player bin

--- bin/enginevsplayer.rs
+++ bin/enginevsplayer.rs
@@ -1,34 +1,57 @@
-use std::io::stdin;
+use std::{env::args, io::stdin};
 
+use anyhow::bail;
 use ultimate_tic_tac_toe::{core::TicTacToe, network::Network, search::Search};
 
-fn input() -> String {
-    let mut result = String::new();
-    stdin().read_line(&mut result).unwrap();
-    result
-}
+fn input(board: &TicTacToe, mut buffer: &mut String) -> u8 {
+    println!("input a valid square move 0 to 80:");
 
-fn main() {
-    let net = Network::load("databin/gen98_weights.bin".to_owned());
-    let mut board = TicTacToe::new();
-    let mut search = Search::new();
-
-    println!("{}", board);
-    while !board.check_win() && !board.is_full() {
-        println!("input a valid square move 0 to 80:");
-        let player_mv = match input().trim().parse::<u8>() {
+    let player_mv;
+    loop {
+        buffer.clear();
+        stdin().read_line(&mut buffer).unwrap();
+        println!("new buffer {}", buffer);
+        player_mv = match buffer.trim().parse::<u8>() {
             Ok(mv) if board.validate_move(mv).is_ok() => mv,
             _ => {
                 println!("Invalid input or move. Try again.");
                 continue;
             }
         };
-        board.make(player_mv);
-        println!("{}", board);
+        break;
+    }
 
-        let engine_mv = search.think(&board, 7, &net);
-        board.make(engine_mv);
+    player_mv
+}
+
+fn main() -> anyhow::Result<()> {
+    let args: Vec<String> = args().collect();
+
+    let net = Network::load(format!("databin/gen{}_weights.bin", args[1]));
+    let mut board = TicTacToe::new();
+    let mut search = Search::new();
+
+    let mut buffer = String::new();
+    println!("Who starts first ? (player || engine)");
+    stdin().read_line(&mut buffer)?;
+
+    let mut turn = match buffer.trim() {
+        "player" => 0,
+        "engine" => 1,
+        _ => bail!("invalid option"),
+    };
+
+    println!("{}", board);
+    while !board.check_win() && !board.is_full() {
+        let mv = match turn {
+            0 => input(&board, &mut buffer),
+            1 => search.think(&board, 7, &net),
+            _ => unreachable!(),
+        };
+
+        board.make(mv);
         println!("{}", board);
+        turn ^= 1;
     }
 
     if board.check_win() {
@@ -36,4 +59,6 @@ fn main() {
     } else {
         println!("draw");
     }
+
+    Ok(())
 }
